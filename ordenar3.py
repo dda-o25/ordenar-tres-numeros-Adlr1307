bc8cc9933186c58ab399c0288cd8335edfc4f0ff
Adlr1307 <andres.jaime@iteso.mx>
2025-09-18 17:37:01 -0600

    Cuarta versión

--- ordenar3.py
+++ ordenar3.py
@@ -4,23 +4,23 @@ Andrés Enrique Jaime de la Rosa 763799
 """
 
 # Entradas
-primern = int(input())
-segundon = int(input())
-tercern = int(input())
+primern = int(input("Escriba el primer número "))
+segundon = int(input("Escriba el segundo número"))
+tercern = int(input("Escriba el tercer número "))
 
 # Proceso
-if primern > segundon and primern> tercern:
-    if segundon > tercern:
+if primern >= segundon and primern>= tercern:
+    if segundon >= tercern:
         print(primern,".*",segundon,".*",tercern,)
     else:
         print(primern,".*",tercern,".*",segundon)
-elif segundon > primern and segundon> tercern:
-    if primern > tercern:
+elif segundon >= primern and segundon>= tercern:
+    if primern >= tercern:
         print(segundon,".*",primern,".*",tercern)
     else:
         print(segundon,".*",tercern,".*",primern)
-elif tercern > segundon and tercern> primern:
-    if primern > segundon:
+elif tercern >= segundon and tercern>= primern:
+    if primern >= segundon:
         print(tercern,".*",primern,".*",segundon)
     else:
         print(tercern,".*",segundon,".*",primern)
